12ecfc9d380f3f85b772fe280b7137091fd3b924
Lia Skalkos <hello@liaskalkos.net>
2015-06-17 17:50:21 -0400

    Beasting SQL

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,64 @@
 -- your code here
+-- 10 projects, 20 users, and 30 pledges into the database.
+
+-- CREATE TABLE projects(
+--   id INTEGER PRIMARY KEY AUTOINCREMENT,
+--   title TEXT,
+--   category TEXT,
+--   funding_goal INTEGER,
+--   start_date TEXT,
+--   end_date TEXT
+-- );
+-- CREATE TABLE users(
+--   id INTEGER PRIMARY KEY AUTOINCREMENT,
+--   name TEXT,
+--   age INTEGER
+-- );
+-- CREATE TABLE pledges(
+--   id INTEGER PRIMARY KEY AUTOINCREMENT,
+--   amount INTEGER,
+--   user_id INTEGER,
+--   project_id INTEGER
+-- );
+
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ('Shelter', 'Volunteer', 200, '01/01/16', '06/01/16'),
+('Shelter', 'Volunteer', 200, '01/01/16', '06/01/16'),
+('Shelter', 'Volunteer', 200, '01/01/16', '06/01/16'),
+('Shelter', 'Volunteer', 200, '01/01/16', '06/01/16'),
+('Shelter', 'Volunteer', 200, '01/01/16', '06/01/16'),
+('Shelter', 'Volunteer', 200, '01/01/16', '06/01/16'),
+('Shelter', 'Volunteer', 200, '01/01/16', '06/01/16'),
+('Shelter', 'Volunteer', 200, '01/01/16', '06/01/16'),
+('Shelter', 'Volunteer', 200, '01/01/16', '06/01/16'),
+('Shelter', 'Volunteer', 200, '01/01/16', '06/01/16');
+
+INSERT INTO users (name, age)
+VALUES ('Bob', 20),
+('Bob', 20),
+('Bob', 20),
+('Bob', 30),
+('Dan', 22),
+('Bob', 25),
+('Bob', 20),
+('Enrique', 20),
+('Bob', 20),
+('Bob', 20),
+('Susan', 20),
+('Bob', 20),
+('Hellen', 20),
+('Bob', 20),
+('Bob', 20),
+('Jim', 20),
+('Bob', 20),
+('Bob', 20),
+('Bob', 20),
+('Ann', 20);
+
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES
+(300, 2, 5), (300, 2, 5), (100, 2, 5), (300, 9, 5), (300, 2, 5), (300, 2, 5), (300, 2, 5), (300, 2, 5), (300, 2, 5), (300, 2, 5), (300, 2, 5), (300, 2, 5), (300, 2, 5), (300, 2, 5), (300, 2, 5), (300, 2, 5), (300, 2, 5), (300, 2, 5), (300, 2, 5),
+  (300, 2, 5), (360, 2, 2), (307, 2, 3), (309, 12, 4), (23, 2, 4), (10, 2, 4), (300, 2, 4), (300, 2, 5), (300, 2, 5), (300, 2, 5), (300, 2, 5);
+
+SELECT sum(amount) AS project_amount FROM pledges GROUP BY project_id;
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,24 +8,78 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts
 "Write your SQL query Here"
+
+<<-SQL
+    SELECT projects.title, sum(pledges.amount)
+    FROM pledges
+    INNER JOIN projects
+    ON projects.id = pledges.project_id
+    GROUP BY projects.title
+  SQL
+
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges
 "Write your SQL query Here"
+
+<<-SQL
+  SELECT users.name, users.age, sum(pledges.amount)
+  FROM pledges
+  INNER JOIN users
+  ON users.id = pledges.user_id
+  GROUP BY users.name ORDER BY users.name;
+SQL
+
 end
 
 def selects_the_titles_of_all_projects_that_have_met_their_funding_goal
 "Write your SQL query Here"
+
+<<-SQL
+
+  SELECT projects.title, projects.funding_goal-sum(pledges.amount)
+  FROM pledges
+  INNER JOIN projects
+  ON projects.id = pledges.project_id
+  GROUP BY projects.title
+  HAVING projects.funding_goal-sum(pledges.amount) <= 0;
+  SQL
+
+
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount
 "Write your SQL query Here"
+
+<<-SQL
+  SELECT users.name, sum(pledges.amount)
+  FROM pledges
+  INNER JOIN users
+  ON users.id = pledges.user_id
+  GROUP BY users.name ORDER BY sum(pledges.amount);
+  SQL
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
 "Write your SQL query Here"
+<<-SQL
+  SELECT projects.category, pledges.amount
+  FROM pledges
+  INNER JOIN projects
+  ON projects.id = pledges.project_id
+  WHERE projects.category = "music";
+  SQL
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_book_category
 "Write your SQL query Here"
+<<-SQL
+
+  SELECT projects.category, sum(pledges.amount)
+  FROM pledges
+  INNER JOIN projects
+  ON projects.id = pledges.project_id
+  WHERE projects.category = 'books';
+
+  SQL
 end
